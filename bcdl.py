1a51c0102a591020d134405ca86f942521677684
unknown <etharner@gmail.com>
2017-10-06 14:30:35 +1000

    Add band and artist extracter

--- bcdl.py
+++ bcdl.py
@@ -3,6 +3,7 @@ from lxml import html
 from lxml.html import parse
 import requests
 import youtube_dl
+import re
 
 class MyLogger(object):
     def debug(self, msg):
@@ -20,7 +21,13 @@ def my_hook(d):
 
 req = requests.request('GET', 'https://random-album.com/')
 page = html.fromstring(req.text)
-url = page.xpath('//a/@href')[0]
+url = str(page.xpath('//a/@href')[0])
+
+re = re.compile('https://([^.]*).*/album/(.*)')
+band = re.search(url).group(1)
+album = re.search(url).group(2)
+
+print('Catching Artist: {0}, Album: {1}'.format(band, album))
 
 ydl_opts = {
     'format': 'best',
@@ -31,7 +38,10 @@ ydl_opts = {
     }],
     'logger': MyLogger(),
     'progress_hooks': [my_hook],
+    'outtmpl': 'downloads/{0} - {1}/%(title)s.%(ext)s'.format(band, album),
 }
 
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download([url])
+
+print('Done!')    
